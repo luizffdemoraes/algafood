1a9b121d00a2ce4b1db9b40c59badfa7f00b6840
Luiz Felipe <lffm1994@gmail.com>
2024-01-07 23:17:01 -0300

    2.12. Injetando dependências (beans Spring)

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller // Classe responsavel por receber requisições
 public class MeuPrimeiroController {
 
+    private AtivacaoClienteService ativacaoClienteService;
+
+    public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+        this.ativacaoClienteService = ativacaoClienteService;
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
+        Cliente joao = new Cliente("João", "joao@xyz.com", "3499998888");
+
+        ativacaoClienteService.ativar(joao);
+
+
         return "Hello";
     }
 }
